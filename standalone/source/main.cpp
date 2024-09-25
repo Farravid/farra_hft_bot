a174cc20a06d5b2287bdc7d140fd56c9a62a165d
David Martínez García <davidmg103@hotmail.com>
2024-09-25 14:22:38 +0200

    We have a curl wrapper working fine

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,54 +1,14 @@
-#include <greeter/greeter.h>
-#include <greeter/version.h>
-
-#include <cxxopts.hpp>
-#include <iostream>
-#include <string>
-#include <unordered_map>
+#include <cpr/cpr.h>
 #include <print>
 
-auto main(int argc, char** argv) -> int {
-  const std::unordered_map<std::string, greeter::LanguageCode> languages{
-      {"en", greeter::LanguageCode::EN},
-      {"de", greeter::LanguageCode::DE},
-      {"es", greeter::LanguageCode::ES},
-      {"fr", greeter::LanguageCode::FR},
-  };
 
-  cxxopts::Options options(*argv, "A program to welcome the world!");
+int main(int argc, char** argv) {
+    cpr::Response r = cpr::Get(cpr::Url{"https://query1.finance.yahoo.com/v8/finance/chart/EURUSD=X?interval=15m&range=1d"});
+    r.status_code;                  // 200
+    r.header["content-type"];       // application/json; charset=utf-8
+    r.text;                         // JSON text string
 
-  std::string language;
-  std::string name;
-
-  // clang-format off
-  options.add_options()
-    ("h,help", "Show help")
-    ("v,version", "Print the current version number")
-    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
-    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
-  ;
-  // clang-format on
-
-  auto result = options.parse(argc, argv);
-
-  if (result["help"].as<bool>()) {
-    std::cout << options.help() << std::endl;
+    std::println("Text: {}", r.text);
     return 0;
-  }
 
-  if (result["version"].as<bool>()) {
-    std::cout << "Greeter, version " << GREETER_VERSION << std::endl;
-    return 0;
-  }
-
-  auto langIt = languages.find(language);
-  if (langIt == languages.end()) {
-    std::cerr << "unknown language code: " << language << std::endl;
-    return 1;
-  }
-
-  greeter::Greeter greeter(name);
-  std::cout << greeter.greet(langIt->second) << std::endl;
-
-  return 0;
 }
