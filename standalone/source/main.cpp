87e6566f13200a7daa1292c6fff4a18da758ccb3
David Martínez García <davidmg103@hotmail.com>
2024-09-25 16:19:54 +0200

    We know how to use json library

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,23 +1,26 @@
 #include <cpr/cpr.h>
 #include <print>
-#include <fstream>
 #include <nlohmann/json.hpp>
-using json = nlohmann::json;
-
-// ...
-
+#include <iostream>
 
 int main(int argc, char** argv) {
     cpr::Response r = cpr::Get(cpr::Url{"https://query1.finance.yahoo.com/v8/finance/chart/EURUSD=X?interval=15m&range=1d"});
     r.status_code;                  // 200
     r.header["content-type"];       // application/json; charset=utf-8
-    r.text;                         // JSON text string
+    r.text; 
+    
 
-    std::println("Text: {}", r.text);
+    nlohmann::json data = nlohmann::json::parse(r.text);
+    auto data2 = data["chart"]["result"][0]["meta"];
 
-    std::ifstream f("example.json");
-    json data = json::parse(f);
+    //std::cout << "Parsed JSON: " << data2.dump(4) << std::endl; 
+
+    std::println("Thing: {}",data2["currency"].get<std::string>());
+
+    //std::println("Fruit: {}", data["fruit"]);
+    //auto m = data["meta"];
+    //std::println("Currency: {}", data["meta"]);
+    //std::println("Text: {}", data);
 
     return 0;
-
 }
